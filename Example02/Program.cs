4a6b0d1129a06b7bbfec81513ed321ede6e7540f
Mikhail <kameel018rus@gmail.com>
2023-01-17 06:30:41 +0300

    Нахождение максимума из 3-х чисел

---
+++ Example02/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число >");
+String numberA = Console.ReadLine();
+int a = Convert.ToInt32(numberA);
+Console.Write("Введите второе число >");
+String numberB = Console.ReadLine();
+int b = Convert.ToInt32(numberB);
+Console.Write("Введите третье число >");
+String numberC = Console.ReadLine();
+int c = Convert.ToInt32(numberC);
+int max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+Console.WriteLine($"{max}");
+
+    
