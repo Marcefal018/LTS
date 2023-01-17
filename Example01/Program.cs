6fee9a480515b7df87346d9fc5010b115dda12e1
Mikhail <kameel018rus@gmail.com>
2023-01-17 05:37:38 +0300

    Нахождение min и max

---
+++ Example01/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число >");
+String numberA = Console.ReadLine();
+int a = Convert.ToInt32(numberA);
+Console.Write("Введите второе число >");
+String numberB = Console.ReadLine();
+int b = Convert.ToInt32(numberB);
+if(a > b)
+{
+     Console.WriteLine($"число {a} = max, число {b} = min");
+}else
+{
+     Console.WriteLine($"число {a} = min, число {b} = max");
+}
