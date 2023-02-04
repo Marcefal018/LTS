874aa917f4f3966b2c07af69a322f9bde0a73250
Mikhail <kameel018rus@gmail.com>
2023-02-04 22:38:39 +0300

    Количество чисел больше 0

---
+++ Example61/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+int InputInt(string massage)
+{
+    System.Console.WriteLine($"{massage}");
+    return int.Parse(Console.ReadLine());   
+}
+
+int[] GenerateArray(int len)
+{
+    int[] array = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(-10, 10);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+    {
+        System.Console.Write($"{item}\t");
+    }
+    System.Console.WriteLine();
+}
+int SumNumber(int[] array )
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if( array[i] > 0)
+        {
+            sum = sum + 1;
+        }
+    }
+    return sum;
+}
+
+int number = InputInt("Введите число >");
+int[] array = GenerateArray(number);
+PrintArray(array);
+System.Console.WriteLine($"Количество положительный чисел = {SumNumber(array)}");
