c219273adb42141bc1fb52d3375692f46fd41ed6
Mikhail <kameel018rus@gmail.com>
2023-02-14 11:30:20 +0300

    Программа, которая упорядочивает по убыванию элементы каждой строки

---
+++ Example81/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочивает
+// по убыванию элементы каждой строки двумерного массива.
+
+int[,] GenerateArray(int line, int column)
+{
+    int[,] matr = new int[line, column];
+    Random rnd = new Random();
+    for (int i = 0; i < line; i++)
+    {
+        for (int j = 0; j < column; j++)
+            matr[i, j] = rnd.Next(1, 10);
+    }
+    return matr;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+void OrderingElement(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+int[,] matrix = GenerateArray(5, 6);
+PrintArray(matrix);
+System.Console.WriteLine();
+OrderingElement(matrix);
+PrintArray(matrix);
